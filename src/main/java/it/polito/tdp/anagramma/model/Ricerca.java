8295dbdc05bad3430077f40b76bede93aa1add6d
Enrico Zappador <s236559@studenti.polito.it>
2020-04-09 16:10:15 +0200

    fine es

--- src/main/java/it/polito/tdp/anagramma/model/Ricerca.java
+++ src/main/java/it/polito/tdp/anagramma/model/Ricerca.java
@@ -2,12 +2,42 @@ package it.polito.tdp.anagramma.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class Ricerca {
 	
+	private List<String> soluzione; 	
+	
 
-	public List<String> anagrammi(String string) {
+	public List<String> anagrammi(String parola) {
+		parola=parola.toUpperCase();
+		this.soluzione = new ArrayList<String>();
+		List<Character> disponibili = new ArrayList<>(); 
+		for (int i = 0; i< parola.length(); i++){
+			disponibili.add(parola.charAt(i));
+		}
 		
-		return null; 
+		cerca("", 0, disponibili); 
+		return this.soluzione; 
           }
+	
+	public void cerca (String parziale, int livello, List<Character> disponibili) {
+		
+		if(disponibili.size()==0) { 
+			
+			this.soluzione.add(parziale);
+			
+		}
+		else {
+			for(Character ch : disponibili) {
+				String tentativo = parziale + ch ; 
+				
+				List<Character> rimanenti = new ArrayList<>(disponibili); 
+				rimanenti.remove(ch); 
+				cerca(tentativo, livello+1, rimanenti); 
+				
+			}
+		}
+		
+	}
 }
